3d7d7a026d7cedb2d00fc834168824f9bf102f75
ogabek2055 <ogabeksodiqov@gmail.com>
2024-03-30 23:04:29 +0500

    get put post delete requests

---
+++ 4-month/4-8/videodars/js/main.js
@@ -0,0 +1,55 @@
+// https://jsonplaceholder.typicode.com/todos
+
+//? get request
+// fetch("https://crudcrud.com/api/e3c10d389fce4b6cbf93b5c8c73c8bb8/users",()=> {
+//     method: "GET"
+// }).then((data) => {
+//     return data.json()
+// }).then((todos) => {
+//     console.log(todos);
+// })
+
+
+
+//? Post request
+// fetch(
+//     "https://crudcrud.com/api/e3c10d389fce4b6cbf93b5c8c73c8bb8/users", {
+//     headers: {
+//         "Content-Type": "application/json"
+//     },
+//     method: "POST",
+//     body: JSON.stringify({
+//         firstname: "Ogajon",
+//         wor: "Pdp",
+//         age: 19,
+//     })
+// }).then(response => {
+//     return response.json()
+// }).then(result => {
+//     console.log(result);
+// })
+
+//?  PUT request
+
+// fetch("https://crudcrud.com/api/e3c10d389fce4b6cbf93b5c8c73c8bb8/users/6607ce8f1492af03e8f10e00", {
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+//     method: "PUT",
+//     body: JSON.stringify({
+//         firstname: "Ismoil",
+//     }),
+// }).then(response => {
+//     console.log(response);
+// })
+
+//? DELETE 
+
+// fetch("https://crudcrud.com/api/e3c10d389fce4b6cbf93b5c8c73c8bb8/users/6607d3301492af03e8f10e2f", {
+//     method: "DELETE"
+// }).then((res) => {
+//     console.log(res);
+// })
+
+
+
