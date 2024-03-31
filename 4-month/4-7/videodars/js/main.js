308f9ceee079bc03aa40e75bec81072760264b3b
ogabek2055 <ogabeksodiqov@gmail.com>
2024-03-31 14:26:03 +0500

    Fetch Async await try catch Promise

--- 4-month/4-7/videodars/js/main.js
+++ 4-month/4-7/videodars/js/main.js
@@ -8,5 +8,49 @@
 
 // console.log("Hello"); 
 
+//? Promise
 
-//? Asynchronous 
+// function getUsername() {
+//     return new Promise((resolve, reject) => {
+//         const username = "Bobur"
+//         if (!username) {
+//             reject(Error("No"))
+//         } else {
+//             resolve(username)
+//         }
+//     })
+// }
+
+// getUsername().then((data) => {
+//     alert(data + "🙄")
+// }).catch(error => {
+//     console.log(error);
+// })
+
+//? fetch()
+//? Promisa steps 1. Pending 2. Fulfilled/Rejected
+
+// fetch("https://jsonplaceholder.typicode.com/photos").then(data => {
+//     return data.json()
+// }).then(photos => {
+//     console.log(photos);
+// }).catch(error => {
+//     console.log(error);
+// }).finally(() => {
+//     console.log("response resieved");
+// })
+
+// async await
+
+async function getPhotos() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/photos")
+        console.log(response);
+        const photos = await response.json()
+        console.log(photos);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getPhotos()
