137a27a1f8107c5426182f1cf82e4e19e90394b5
ogabek2055 <ogabeksodiqov@gmail.com>
2024-04-26 17:22:40 +0500

    Prototypal inheritance

---
+++ 5-month/5-4/js/main.js
@@ -0,0 +1,28 @@
+
+//! Inheritance 
+// class Car {
+//   constructor(speed) {
+//     this.speed = speed
+//   }
+
+//   drive() {
+//     console.log(`Car is driving ${this.speed}`);
+//   }
+// }
+
+
+// class SportsCar extends Car {
+//   constructor(speed, nitro) {
+//     super(speed)
+//     this.nitro = nitro;
+//   }
+// }
+
+// const Ferrari = new SportsCar(330, true)
+// console.log(Ferrari); 
+
+//! Method Chaining
+//  [0,1,2,3,4,5,6,7,8,9].map(() => 2).forEach((num)=> {
+//    console.log(num);
+
+//  })
