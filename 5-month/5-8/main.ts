0bb2c22693e1d46df3dda7ff14ffde292122df08
ogabek2055 <ogabeksodiqov@gmail.com>
2024-05-04 14:00:08 +0500

    typeScript ga kirish

---
+++ 5-month/5-8/main.ts
@@ -0,0 +1,62 @@
+//? primitive types 
+
+// var username: any = "Ibrohim"; // any typesida hohlagan type ga otadi
+
+// username = 10 + 2;
+// username = "John"
+
+// console.log(username);
+
+//? Litral types
+
+// let s:  "one" = "one";
+// let z = num = num
+// console.log(s);
+
+//? union types
+// let ages: number | string
+
+// ages = "Ogabek"
+// console.log(ages);
+
+//?  Array
+// const users: number[] = [1,2,3,4,5,6,7,8,9]
+// console.log(users);
+// const users2: (string | number)[] = [1,2,3,4,5,6,7,8,9]
+// console.log(users2);
+
+//? Tuple
+
+// const person: [string, number, boolean] = ["Ogabek", 19 , true]
+// console.log(person);
+
+//? Named tuple
+//  const person: [username:string, age:number, isMarried:boolean] = ["Ogabek", 19 , true]
+// console.log(person);
+
+//? Object
+//  const user: {
+//     name: string,
+//     age: number,
+//     phone: string
+//  } = {
+//     name: "ogabek",
+//     age: 19,
+//     phone: "123i123"
+//  }
+//  console.log(user);
+ 
+//? Object optional property
+//  const user: {
+//     name: string,
+//     age: number,
+//     phone?: string
+    //    car?: string,
+//  } = {
+//     name: "ogabek",
+//     age: 19,
+//     // phone: "123i123"
+//  }
+// user.push("moshina")
+//  console.log(user);
+ 
